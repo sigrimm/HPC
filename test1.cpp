b1895d832c69f424c97cae2afc8043829e60edd5
Simon Grimm <simon.l.grimm@gmail.com>
2025-05-11 17:34:49 +0200

    added timing

--- test1.cpp
+++ test1.cpp
@@ -1,41 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <chrono>
 
 
 
 
 
-void addTest(double *a, double *b, double *c, const int N){
+void addTest(double *a, double *b, double *c, const int N, int &time){
 
-  for(int i = 0; i < N; ++i){
-    c[i] = a[i] + b[i];
-  }
+	std::chrono::steady_clock::time_point time_begin = std::chrono::steady_clock::now();
+
+	for(int t = 0; t < 10000000; ++t){
+		for(int i = 0; i < N; ++i){
+			c[i] += a[i] + b[i];
+		}
+	}
+        std::chrono::steady_clock::time_point time_end = std::chrono::steady_clock::now();
+        time += std::chrono::duration_cast<std::chrono::microseconds>(time_end - time_begin).count();
 }
 
 
 int main(){
 
-  int N = 100;
+	int N = 100;
+	int time = 0;
 
-  double *a, *b, *c;
-
-
-  a = (double*)malloc(N * sizeof(double));
-  b = (double*)malloc(N * sizeof(double));
-  c = (double*)malloc(N * sizeof(double));
-
-  for(int i = 0; i < N; ++i){
-    a[i] = 0.1;
-    b[i] = 100.0;
-    c[i] = 0.0;
-  }
-
-  addTest(a, b, c, N);
-
-  for(int i = 0; i < N; ++i){
-    printf("%d %g\n", i, c[i]);
-  }
+	double *a, *b, *c;
 
 
-  return 0;
+	a = (double*)malloc(N * sizeof(double));
+	b = (double*)malloc(N * sizeof(double));
+	c = (double*)malloc(N * sizeof(double));
+
+	for(int i = 0; i < N; ++i){
+		a[i] = 0.1;
+		b[i] = 100.0;
+		c[i] = 0.0;
+	}
+
+	addTest(a, b, c, N, time);
+
+
+	printf("Time in seconds:  %.8g\n", time / 1000000.0);
+
+	//for(int i = 0; i < N; ++i){
+	//	printf("%d %g\n", i, c[i]);
+	//}
+
+
+	return 0;
 }
